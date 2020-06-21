65d95bd4dc796861eaed76e01c937d0b1df430a5
JuanSanz11 <juandavidsanzalmanza@outlook.com>
2020-06-21 17:10:44 -0300

    colprimarios

---
+++ README.md
@@ -0,0 +1,10 @@
+Colores primarios
+
+Primary Colors
+
+
+head
+    
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />  hace que barra de navegacion siempre vuelva al inicio.
+    
+head
---
+++ player.js
@@ -0,0 +1,58 @@
+var amarillo = document.getElementById("amarillo")
+var v = document.getElementsByTagName("audio")[0];
+var sound = false;
+
+amarillo.addEventListener("click", function(){
+      v.play();
+      sound = true;
+   });
+
+var azul = document.getElementById("azul")
+var f = document.getElementsByTagName("audio")[1];
+var sound = false;
+
+azul.addEventListener("click", function(){
+      f.play();
+      sound = true;
+   });
+
+   var rojo = document.getElementById("rojo")
+   var g = document.getElementsByTagName("audio")[2];
+   var sound = false;
+   
+   rojo.addEventListener("click", function(){
+         g.play();
+         sound = true;
+      });
+
+
+
+
+      var yellow = document.getElementById("yellow")
+      var v = document.getElementsByTagName("audio")[0];
+      var sound = false;
+      
+      yellow.addEventListener("click", function(){
+            v.play();
+            sound = true;
+         });
+      
+      var blue = document.getElementById("blue")
+      var f = document.getElementsByTagName("audio")[1];
+      var sound = false;
+      
+      blue.addEventListener("click", function(){
+            f.play();
+            sound = true;
+         });
+      
+         var red = document.getElementById("red")
+         var g = document.getElementsByTagName("audio")[2];
+         var sound = false;
+         
+         red.addEventListener("click", function(){
+               g.play();
+               sound = true;
+            });
+      
+
